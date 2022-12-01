4ca96a90cad63e207cd6f68a94c42fa104b27512
Alejandro Osornio <50227494+AOx0@users.noreply.github.com>
2022-12-01 11:21:42 -0600

    Include FPS target in settings

--- Assets/Scripts/Graphics/TargetFrameRate.cs
+++ Assets/Scripts/Graphics/TargetFrameRate.cs
@@ -1,22 +1,59 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
 public class TargetFrameRate : MonoBehaviour {
 	public TMP_Dropdown vSyncRate;
-	
+	public TMP_Dropdown FpsTarget;
+
 	void Awake () {
 	    // Its a dead simple app. There's no need for 120 fps
 	    // By default the vSyncCount is 4 (1/4 of max fps, ex. 120/4 = 30)
-	    vSyncRate.value = PlayerPrefs.GetInt("vSyncRate", 3);
-	    QualitySettings.vSyncCount = vSyncRate.value + 1;
+
+	    // We opt to use vSync by default with vSyncCount of 1/4
+	    vSyncRate.value = PlayerPrefs.GetInt("vSyncRate", 4);
+	    QualitySettings.vSyncCount = vSyncRate.value;
+	    
+	    // By default TargetFrames is 0
+	    FpsTarget.value = PlayerPrefs.GetInt("fpsTarget", 0);
+	    Application.targetFrameRate =  FpsTarget.value != 0 ? FpsTarget.value * 10 : -1;
 	}
 	
 	public void SetVSyncRatio(System.Int32 value)
 	{
+		// Clear fpsTarget
+		PlayerPrefs.SetInt("fpsTarget", 0);
+		FpsTarget.value = 0;
+		Application.targetFrameRate = -1;
+		
+		if (value == 0)
+		{
+			SetFpsTarget(3);
+		}
+		
 		PlayerPrefs.SetInt("vSyncRate", value);
-		QualitySettings.vSyncCount = value + 1;
 		vSyncRate.value = value;
+		
+		QualitySettings.vSyncCount = value;
+	}
+
+	public void SetFpsTarget(System.Int32 value)
+	{
+		// Clear vSync Count
+		PlayerPrefs.SetInt("vSyncRate", 0);
+		vSyncRate.value = 0;
+		QualitySettings.vSyncCount = 0;
+		
+		if (value == 0)
+		{
+			SetVSyncRatio(4);
+		}
+		
+		PlayerPrefs.SetInt("fpsTarget", value);
+		FpsTarget.value = value;
+		
+		Application.targetFrameRate = value != 0 ? value * 10 : -1;
 	}
 }
